df1756365db7a05087892e59e2a63f0087733fed
Ben Toups <ben@toupsonline.com>
2019-09-03 14:20:00 -0500

    Basic working model. Plots the allele freqs. of one population over time

--- PopG_test.py
+++ PopG_test.py
@@ -10,14 +10,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-pop = int(input('What is the size of the population? '))
+os.system('cls')
+
+N = int(input('What is the size of the population? '))
 freqA = float(input('What is the starting frequency of the A allele? '))
+gens = int(input('How many generations is the population simulated over? '))
 
 
-# setting up the random number generator
-
-
-def randnum():
+def randnum():      # random number generator
     num1 = random.randint(1, (2 ** 15 - 1))
     num2 = random.randint(1, (2 ** 15 - 1))
     numer = (num1 * 32768) + num2
@@ -25,10 +25,12 @@ def randnum():
     return choice
 
 
-# Generating individuals
+def PickParents():    # selects a parent from the population
+    num = (randnum() * N) // 1
+    return num
 
 
-def GenIndiv():
+def GenIndiv():    # Generates individuals
     first = randnum()
     if first <= freqA:
         first = 1
@@ -47,19 +49,54 @@ def GenIndiv():
         return "aa"
 
 
-indivs = []
+def calcFreqs(pops):    # calculates frequencies
+    A = 0
+    for i in pops:
+        if i == 'AA':
+            A += 2
+        elif i == 'Aa' or i == 'aA':
+            A += 1
+        else:
+            pass
+    frequency = A / (N * 2)
+    return frequency
 
-for i in range(pop):
+
+indivs = []
+freqs = []
+
+for i in range(N):   # generates an initial population
     indivs.append(GenIndiv())
-A = 0
-a = 0
-for i in indivs:
-    if i == "AA":
-        A += 2
-    elif i == "Aa":
-        A += 1
-        a += 1
-    else:
-        a += 2
-print("Frequency of A is ", (A / (A + a)))
-print("Frequency of a is ", (a / (A + a)))
+freqs.append(calcFreqs(indivs))
+
+g = 1
+while g != gens:
+    newGen = []
+    for i in range(N):
+        first = int(PickParents())
+        second = int(PickParents())
+        while first == second:
+            second = int(PickParents())
+        firstparent = indivs[first]
+        secondparent = indivs[second]
+        ran1 = randnum()
+        if ran1 < .5:
+            firstgamete = firstparent[0]
+        else:
+            firstgamete = firstparent[1]
+        ran2 = randnum()
+        if ran2 < .5:
+            secondgamete = secondparent[0]
+        else:
+            secondgamete = secondparent[1]
+        newAllele = firstgamete + secondgamete
+        newGen.append(newAllele)
+    freqs.append(calcFreqs(newGen))
+    indivs = newGen
+    g += 1
+
+
+plt.plot(range(gens), freqs)
+plt.xlabel('Generation')
+plt.ylabel('p(A)')
+plt.show()
