916538a95a2bf014212d537504c8aabdaa6abb76
Ben <ben@toupsonline.com>
2020-08-28 10:15:55 -0500

    Final code

--- metadata.py
+++ metadata.py
@@ -1,30 +1,39 @@
 import os
 import glob
 import datetime
-path = 'C:/Users/ben/Desktop/General/Documents/misc/'
-
+import csv
+path = 'C:/Users/ben/Desktop/General/Documents/'
+outfile = 'C:/Users/ben/Desktop/metadata.csv'
 
 files = os.listdir(path)
 
+with open(outfile, 'a') as fp:
+    headers = ['File Name', 'File Type', 'Date Modified']
+    wr = csv.writer(fp, dialect = 'excel', lineterminator = '\n')
+    wr.writerow(headers)
 
-class file:
+
+class infile:
     def __init__(self, name, ftype, date):
         self.name = name
         self.ftype = ftype
         self.date = date
-    def printdata(self):
+
+    def printData(self):
         print('File name: {}, File Type: {}, Date Modified: {}'.format(self.name, self.ftype, self.date))
 
-#mtime = os.stat(path + files[1]).st_mtime
-#timestamp = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d-%H:%M')
-#print(timestamp)
+    def printToCSV(self, csvfile):
+        data = [self.name, self.ftype, self.date]
+        with open(csvfile, 'a') as fp:         
+            wr = csv.writer(fp, dialect='excel', lineterminator = '\n')
+            wr.writerow(data)
 
 fileobjs = []
 
-for i in range(len(files)):
-    fileobjs.append(file(name = os.path.splitext(files[i])[0], ftype = os.path.splitext(files[i])[1], 
-    date = datetime.datetime.fromtimestamp(os.stat(path+files[i]).st_mtime).strftime('%Y-%m-%d-%H:%M')))
+for i in files:
+    fileobjs.append(infile(name = os.path.splitext(i)[0], ftype = os.path.splitext(i)[1], 
+    date = datetime.datetime.fromtimestamp(os.stat(path+i).st_mtime).strftime('%Y-%m-%d-%H:%M')))
 
 for i in fileobjs:
-    i.printdata()
+    i.printToCSV(outfile)
 
